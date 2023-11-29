15486005a60de8ff8843563a9b59d74e0b98da65
calebtripp <calebtripp@outlook.com>
2023-11-29 10:36:21 -0500

    5 xc 1 with creating custom component with styling.

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,14 +4,39 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-const smallBox = (
-  <div
-    className={'box box--small'}
-    style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
-  >
-    small lightblue box
-  </div>
-)
+function Box(props) {
+  return (
+    <div className={`box ${props.className}`} style={props.style}>
+      {props.children}
+    </div>
+  )
+}
+
+// const smallBoxTwo = Box( // this does not work.
+//   'box box--small',
+//   {
+//     backgroundColor: 'lightblue',
+//     fontStyle: 'italic',
+//   },
+//   'small lightblue box two',
+// )
+
+const smallBoxFour = Box({
+  // this works.
+  className: 'box box--small',
+  style: {backgroundColor: 'lightblue', fontStyle: 'italic'},
+  children: 'small lightblue box 4',
+})
+
+const smallBox = // this works.
+  (
+    <div
+      className={'box box--small'}
+      style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
+    >
+      small lightblue box
+    </div>
+  )
 const mediumBox = (
   <div
     className={'box box--medium'}
@@ -28,11 +53,33 @@ const largeBox = (
     large orange box
   </div>
 )
+// this is the test for the exercise and produces as intended.
+const element = (
+  <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+    small lightblue box 5
+  </Box>
+)
 
 function App() {
   return (
     <div>
       {smallBox}
+      {/*{smallBoxTwo} this does not work. */}
+      {/*{Box( this does not work.*/}
+      {/*  'box box--small',*/}
+      {/*  {*/}
+      {/*    backgroundColor: 'lightblue',*/}
+      {/*    fontStyle: 'italic',*/}
+      {/*  },*/}
+      {/*  'small lightblue box two',*/}
+      {/*)}*/}
+      {Box({
+        className: 'box box--small',
+        style: {backgroundColor: 'lightblue', fontStyle: 'italic'},
+        children: 'small lightblue box 3',
+      })}
+      {smallBoxFour}
+      {element}
       {mediumBox}
       {largeBox}
     </div>
